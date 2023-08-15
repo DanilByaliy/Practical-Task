572f702ccdbe19bc664681f6350f1411526c705e
DanilByaliy <byaliy03@gmail.com>
2023-08-15 21:44:09 +0300

    feat(tickets): add getting sections when initializing the module

--- server/src/tickets/tickets.module.ts
+++ server/src/tickets/tickets.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TicketsController } from './tickets.controller';
 import { TicketsService } from './tickets.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [
+    HttpModule.registerAsync({
+      useFactory: () => ({
+        timeout: 5000,
+        maxRedirects: 5,
+      }),
+    }),
+  ],
   controllers: [TicketsController],
   providers: [TicketsService],
 })
--- server/src/tickets/tickets.service.ts
+++ server/src/tickets/tickets.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { map } from 'rxjs';
+
+interface Section {
+  Id: number;
+  Description: string;
+}
 
 @Injectable()
-export class TicketsService {}
+export class TicketsService implements OnModuleInit {
+  sections: object = {};
+
+  constructor(private readonly httpService: HttpService) {}
+
+  async onModuleInit() {
+    this.httpService
+      .get(
+        'https://my.laphil.com/en/rest-proxy/ReferenceData/Sections?seatMapId=12',
+      )
+      .pipe(
+        map((res) => res.data as Array<Section>),
+        map((sections) => {
+          sections.forEach(({ Id, Description }) => {
+            this.sections[Id] = Description;
+          });
+        }),
+      );
+  }
+}
