4c50c3e320c033de9ac9ff86a405421ce7e34376
DanilByaliy <byaliy03@gmail.com>
2023-08-15 22:12:04 +0300

    feat(tickets): add an endpoint to get available tickets

--- server/src/tickets/tickets.controller.ts
+++ server/src/tickets/tickets.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { TicketsService } from './tickets.service';
 
 @Controller('tickets')
-export class TicketsController {}
+export class TicketsController {
+  constructor(private readonly ticketsService: TicketsService) {}
+
+  @Get('/:eventId')
+  getTickets(@Param('eventId') eventId: string) {
+    return this.ticketsService.getTickets(eventId);
+  }
+}
