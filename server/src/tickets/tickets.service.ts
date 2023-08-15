46251c4fc53e4dd7729f3fd04fec54b743d052aa
DanilByaliy <byaliy03@gmail.com>
2023-08-15 21:53:09 +0300

    feat(tickets): add a method to get available tickets

--- server/src/tickets/tickets.service.ts
+++ server/src/tickets/tickets.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { map } from 'rxjs';
+import { map, firstValueFrom } from 'rxjs';
 
 interface Section {
   Id: number;
@@ -26,5 +26,36 @@ export class TicketsService implements OnModuleInit {
           });
         }),
       );
+  }
+
+  async getTickets(eventId: string) {
+    const seats = await firstValueFrom(
+      this.httpService
+        .get(
+          `https://my.laphil.com/en/rest-proxy/TXN/Packages/${eventId}/Seats?constituentId=0&modeOfSaleId=26&packageId=${eventId}`,
+        )
+        .pipe(map((res) => res.data)),
+    );
+
+    const prices = await firstValueFrom(
+      this.httpService
+        .get(
+          `https://my.laphil.com/en/rest-proxy/TXN/Packages/${eventId}/Prices?expandPerformancePriceType=&includeOnlyBasePrice=&modeOfSaleId=26&priceTypeId=&sourceId=30885`,
+        )
+        .pipe(map((res) => res.data)),
+    );
+
+    return seats
+      .filter(({ SeatStatusId }) => !SeatStatusId)
+      .map(({ SectionId, SeatRow, SeatNumber, ZoneId }) => {
+        const Description = this.sections[SectionId];
+        const { Price } = prices.find(({ ZoneId: id }) => id === ZoneId);
+        return {
+          Section: Description,
+          Row: SeatRow,
+          SeatNumber,
+          Price,
+        };
+      });
   }
 }
